3ed681f385dca3a6258451e0a7e5ff4855de276f
Harshita Mathur <hmathur0797@gmail.com>
2021-02-04 19:29:05 +0530

    separate component create

---
+++ components/sideMenu.js
@@ -0,0 +1,12 @@
+import {Fragment} from 'react';
+const SideMenu = () => ( 
+	<Fragment>
+		<h1 className="my-4">Shop Name</h1>
+			<div className="list-group">
+				<a href="#" className="list-group-item">Category 1</a>
+				<a href="#" className="list-group-item">Category 2</a>
+				<a href="#" className="list-group-item">Category 3</a>
+			</div>
+	</Fragment>
+)
+export default SideMenu;
---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const About = () => {
+    const a = 'Hello WOrld'
+    return(
+        <h1>{a}</h1>
+    )
+  }  
+
+export default About
