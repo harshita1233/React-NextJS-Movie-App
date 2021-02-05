5ad9c6af78ac77104f2d5b314ade4ccb4e670080
Harshita Mathur <hmathur0797@gmail.com>
2021-02-06 02:37:33 +0530

    button added for caraousel>changes required

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,11 +1,14 @@
 import {Fragment} from 'react';
-const SideMenu = () => ( 
+const SideMenu = ({count}) => ( 
 	<Fragment>
 		<h1 className="my-4">Shop Name</h1>
 			<div className="list-group">
 				<a href="#" className="list-group-item">Category 1</a>
 				<a href="#" className="list-group-item">Category 2</a>
 				<a href="#" className="list-group-item">Category 3</a>
+			</div>
+			<div>
+				<h1>{count}</h1>
 			</div>
 	</Fragment>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Navbar from '../components/navbar'
 import SideMenu from '../components/sideMenu'
 import Carousel from '../components/carousel'
 import MovieList from '../components/movieList'
 import Footer from '../components/footer'
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </Head>
-    <Navbar/>
-    <div className = 'home-page'>
-    <div className="container">
-    <div className="row">
-      <div className="col-lg-3">
-        <SideMenu/>
-      </div>
+const Home = () =>{
+  
+  const [count, setCount] = useState(0);
 
-      <div className="col-lg-9">
-       <Carousel/>
-        <div className="row">
-          <MovieList/>
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1)
+  }
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1)
+  }
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      </Head>
+      <Navbar/>
+      <div className = 'home-page'>
+        <div className="container">
+          <button onClick={increment} className="btn btn-primary">Increment</button>
+          <button onClick={decrement} className="btn btn-primary">Decrement</button>
+          <div className="row">
+            <div className="col-lg-3">
+              <SideMenu count={count} />
+            </div>
+            <div className="col-lg-9">
+              <Carousel/>
+              <div className="row">
+                <MovieList/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <Footer/>
+      <style jsx>{`
+        .home-page{
+          padding-top:80px;
+        }
+      `}</style>
     </div>
-  </div>
-  </div>
-  <Footer/>
-  <style jsx>{`
-    .home-page{
-      padding-top:80px;
-    }
-  `}</style>
-  </div>
-)
-
+  )
+  }
 export default Home
